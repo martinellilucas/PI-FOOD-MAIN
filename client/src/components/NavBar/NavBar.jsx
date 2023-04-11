228bae2b25c38ebd412022c94746f18935441859
Lucas Martinelli <martinellilucas8@gmail.com>
2023-04-11 18:47:59 -0300

    carrousel en home y cambio de estilos

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import style from "./NavBar.module.css";
+import image from "../../assets/images/heathy eating.png";
 
 const NavBar = () => {
   return (
     <div className={style.navBar}>
+      <Link to="/home">
+        <img className={style.logo} alt="logo" src={image}></img>
+      </Link>
       <div className={style.menuContainer}>
         <Link to="/home" className={style.menuText}>
           Home
